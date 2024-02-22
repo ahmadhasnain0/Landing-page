3c6b51681e7669436c878a242313f62231ed7c29
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-02-23 00:00:27 +0500

    header

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,18 +2,15 @@
     <div class="container-fluid bg-primary">
   <div class="container px-3 px-sm-0 w-100 align-items-center ">
     <div class="row ">
-      <div class="col-12"><h1 class="text-white roboto-black f-170">FREELANCERS</h1></div>
+      <div class="col-12  px-0 px-sm-2"><h1 class="text-white roboto-black f-170">FREELANCERS</h1></div>
       <div class="col-12 col-md-5">
         <div class="row">
             <div class="col-12 px-0">
-
                 <div class="bar bg-white align-items-center">
                    <img class="voice mb-3 " src="../assets/images/header/svg/search-svgrepo-com.svg" title="Search by Voice"> 
       <input class="searchbar border-0 w-75 ms-3" type="text" title="Search" placeholder="search">
       <a href="#" class="float-end btn bg-black text-white me-1 mt-1 px-2 next-button border border-black rounded-circle d-none d-xl-inline ">→</a>
-
        <a class="float-end  me-3 me-lg-3 mt-1"><img class="voice  " src="../assets/images/header/svg/mic-svgrepo-com.svg" title="Search by Voice"></a>
-
     </div>
             </div>
         </div>
