75c4ba56f8a9a9e9d1ab3edcac7004d6add701fa
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-02-23 16:48:40 +0500

    talent

--- src/pages/landing-page.vue
+++ src/pages/landing-page.vue
@@ -2,11 +2,13 @@
     <Nav />
     <Header/>
     <Services />
+    <Talent />
 </template>
 
 <script setup>
 import Nav from "@/src/components/nav.vue";
 import Header from "@/src/components/header.vue";
 import Services from "@/src/components/popular-services.vue";
+import Talent from "@/src/components/talent.vue";
 
 </script>
