7e4b7424356e3e7221fd59c9991ad2b1b8085aff
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-02-23 13:20:35 +0500

    services

--- src/components/popular-services.vue
+++ src/components/popular-services.vue
@@ -2,7 +2,6 @@
       <div class="container-fluid bg-white mt-0 mt-md-5 py-5">
     <div class="container px-3 px-sm-0 w-100 align-items-center">
       <div class="row">
-        
         <div class="col-12 col-md-8">
             <h1 class="fw-bolder text-uppercase text-black f-62">Popolar services</h1>
             <p class=" me-0 pe-0 me-lg-5 pe-lg-5">Freelancer offer the diverse range of popular services,form web development to conttent writting depending upon client need</p>
@@ -10,9 +9,52 @@
         <div class="col-4 justify-content-end d-md-flex d-none ">
             <a href="#" class="btn bg-black  mx-1 border border-black rounded-circle circled"><img src="../assets/images/popular-services/svg/left-arrow.svg" ></a>
             <a href="#" class="btn bg-white mx-1 border border-black rounded-circle circled"><img src="../assets/images/popular-services/svg/right-arrow.svg" > </a>
-
         </div>
+        <div class="col-12">
+            <div class="row rounded-top">
+            <div class="col-3 p-3 mx-0 border border-gray border-top-left">
+                <h4 class="fw-bolder text-black text-uppercase">3d artist</h4>
+                <a
+                    href="#"
+                    class="btn bg-secondary px-3 m-1 px-md-4 py-1 border-0 rounded-pill"
+                    > database</a
+                  >
+                  <a
+                    href="#"
+                    class="btn bg-secondary px-3  m-1 px-md-4 py-1 border-0 rounded-pill"
+                    > database</a
+                  >
+                  <a
+                    href="#"
+                    class="btn bg-secondary px-3  m-1 px-md-4 py-1 border-0 rounded-pill"
+                    > database</a
+                  >
+            </div>
+            <div class="col-3 px-0 mx-0 border ">
+                <img src="../assets/images/popular-services/img-1.png" width="100%" height="100%" alt="pic1" >
+            </div>
+            <div class="col-3 p-3 mx-0 border border-gray"> <h4 class="fw-bolder text-black text-uppercase">3d artist</h4>
+                <a
+                    href="#"
+                    class="btn bg-secondary px-3  m-1  px-md-4 py-1 border-0 rounded-pill"
+                    > database</a
+                  >
+                  <a
+                    href="#"
+                    class="btn bg-secondary px-3  m-1  px-md-4 py-1 border-0 rounded-pill"
+                    > database</a
+                  >
+                  <a
+                    href="#"
+                    class="btn bg-secondary px-3  m-1  px-md-4 py-1 border-0 rounded-pill"
+                    > database</a
+                  ></div>
+            <div class="col-3 px-0 mx-0 border border-top-right">
+                <img src="../assets/images/popular-services/img-2.png" width="100%" height="100%" alt="pic2" >
 
+            </div>
+        </div>
+        </div>
       </div>
       </div>
       </div>
