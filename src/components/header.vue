6ec8e7a3691b920a2913b38cb7f83a1414508794
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-02-23 12:29:13 +0500

    header

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,27 +1,110 @@
 <template>
-    <div class="container-fluid bg-primary">
-  <div class="container px-3 px-sm-0 w-100 align-items-center ">
-    <div class="row ">
-      <div class="col-12  px-0 px-sm-2"><h1 class="text-white roboto-black f-170">FREELANCERS</h1></div>
-      <div class="col-12 col-md-5">
-        <div class="row">
-            <div class="col-12 px-0">
-                <div class="bar bg-white align-items-center">
-                   <img class="voice mb-3 " src="../assets/images/header/svg/search-svgrepo-com.svg" title="Search by Voice"> 
-      <input class="searchbar border-0 w-75 ms-3" type="text" title="Search" placeholder="search">
-      <a href="#" class="float-end btn bg-black text-white me-1 mt-1 px-2 next-button border border-black rounded-circle d-none d-xl-inline ">→</a>
-       <a class="float-end  me-3 me-lg-3 mt-1"><img class="voice  " src="../assets/images/header/svg/mic-svgrepo-com.svg" title="Search by Voice"></a>
-    </div>
+  <div class="container-fluid bg-primary pb-5">
+    <div class="container px-3 px-sm-0 w-100 align-items-center">
+      <div class="row">
+        <div class="col-12 px-0 px-sm-2">
+          <h1 class="text-white roboto-black f-170">FREELANCERS</h1>
+        </div>
+        <div class="col-12 col-lg-6">
+          <div class="row">
+            <div class="col-11 px-0">
+              <div class="bar bg-white align-items-center">
+                <img
+                  class="voice mb-3"
+                  src="../assets/images/header/svg/search-svgrepo-com.svg"
+                  title="Search by Voice"
+                />
+                <input
+                  class="searchbar border-0 w-75 ms-3"
+                  type="text"
+                  title="Search"
+                  placeholder="search"
+                />
+                <a
+                  href="#"
+                  class="float-end btn bg-black text-white me-1 mt-1 px-2 next-button border border-black rounded-circle d-none d-xl-inline"
+                  >→</a
+                >
+                <a class="float-end me-3 me-lg-3 mt-1"
+                  ><img
+                    class="voice"
+                    src="../assets/images/header/svg/mic-svgrepo-com.svg"
+                    title="Search by Voice"
+                /></a>
+              </div>
             </div>
+            <div class="col-11 px-0">
+              <div class="row px-2 mt-2 ">
+                <div class="col-3">
+                  <p>Popular Skills:</p>
+                </div>
+                <div class="col-3">
+                  <a
+                    href="#"
+                    class="btn bg-secondary px-3 px-md-4 py-1 border-0 rounded-pill"
+                    > database</a
+                  >
+                </div>
+                <div class="col-3">
+                  <a
+                    href="#"
+                    class="btn bg-secondary px-3 px-md-4 py-1 border-0 rounded-pill"
+                    >database </a
+                  >
+                </div>
+                <div class="col-3">
+                  <a
+                    href="#"
+                    class="btn bg-secondary px-3 px-md-4 py-1 border-0 rounded-pill"
+                    >database</a
+                  >
+                </div>
+                
+              </div>
+            </div>
+            <div class="col-10 px-0 mt-3">
+                <p>A freelancers web portal connect bussiness with freelancers, help in hiring and project collaboration </p>
+            </div>
+            <div class="col-12 col-md-9 px-0 mt-3">
+                <div class="card rounded-4">
+      <div class="card-body p-3 pb-0">
+<div class="row">
+<div class="col-12 col-sm-8"><h5 class="card-title">Special title treatment</h5>
+<img src="../assets/images/header/card1.png" width="auto" height="auto" alt="persons-pic" class="mt-2">
+
+</div>
+<div class="col-12 col-sm-4 mt-2">
+    <div>
+    <span class="fa fa-star checked mx-1"></span>
+<span class="fa fa-star checked mx-1"></span>
+<span class="fa fa-star checked mx-1"></span>
+<span class="fa fa-star checked mx-1"></span>
+<span class="fa fa-star checked mx-1"></span></div>
+
+<div class=" mt-4">
+   <h4 class="fw-bold">200+</h4> 
+   <p class="f-12">satisfied customer</p>
+</div>
+</div>
+
+</div>
+
+    </div>
+</div>
+          </div>
+          </div>
         </div>
+        <div class="col-12 col-md-6 d-none d-lg-inline">
+            
+<img src="../assets/images/header/header-pic-removebg-preview.png" width="100%" height="100%" alt="header-pic" class="header-pic position-absolute">
+
+
+
         </div>
-      <div class="col-12 col-md-7">tyrt</div>
+      </div>
     </div>
   </div>
-</div>
 </template>
 <script setup>
-
-import"@/src/assets/scss/components/_header.scss";
-
+import "@/src/assets/scss/components/_header.scss";
 </script>
