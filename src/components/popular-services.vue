77d1714e478caba89683b578e3ce1a13c87c5955
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-02-23 15:21:42 +0500

    talent

--- src/components/popular-services.vue
+++ src/components/popular-services.vue
@@ -1,63 +1,225 @@
 <template>
-      <div class="container-fluid bg-white mt-0 mt-md-5 py-5">
+  <div class="container-fluid bg-white mt-0 mt-md-5 py-5">
     <div class="container px-3 px-sm-0 w-100 align-items-center">
       <div class="row">
         <div class="col-12 col-md-8">
-            <h1 class="fw-bolder text-uppercase text-black f-62">Popolar services</h1>
-            <p class=" me-0 pe-0 me-lg-5 pe-lg-5">Freelancer offer the diverse range of popular services,form web development to conttent writting depending upon client need</p>
+          <h1 class="fw-bolder text-uppercase text-black f-62">
+            Popolar services
+          </h1>
+          <p class="me-0 pe-0 me-lg-5 pe-lg-5">
+            Freelancer offer the diverse range of popular services,form web
+            development to conttent writting depending upon client need
+          </p>
         </div>
-        <div class="col-4 justify-content-end d-md-flex d-none ">
-            <a href="#" class="btn bg-black  mx-1 border border-black rounded-circle circled"><img src="../assets/images/popular-services/svg/left-arrow.svg" ></a>
-            <a href="#" class="btn bg-white mx-1 border border-black rounded-circle circled"><img src="../assets/images/popular-services/svg/right-arrow.svg" > </a>
+        <div class="col-4 justify-content-end d-md-flex d-none">
+          <a
+            href="#"
+            class="btn bg-black mx-1 border border-black rounded-circle circled"
+            ><img
+              src="../assets/images/popular-services/svg/left-arrow.svg"
+              width="100%"
+              height="100%"
+              alt="pic"
+          /></a>
+          <a
+            href="#"
+            class="btn bg-white mx-1 border border-black rounded-circle circled"
+            ><img
+              src="../assets/images/popular-services/svg/right-arrow.svg"
+              width="100%"
+              height="100%"
+              alt="pic"
+            />
+          </a>
         </div>
         <div class="col-12">
-            <div class="row rounded-top">
-            <div class="col-3 p-3 mx-0 border border-gray border-top-left">
-                <h4 class="fw-bolder text-black text-uppercase">3d artist</h4>
-                <a
-                    href="#"
-                    class="btn bg-secondary px-3 m-1 px-md-4 py-1 border-0 rounded-pill"
-                    > database</a
-                  >
-                  <a
-                    href="#"
-                    class="btn bg-secondary px-3  m-1 px-md-4 py-1 border-0 rounded-pill"
-                    > database</a
-                  >
-                  <a
-                    href="#"
-                    class="btn bg-secondary px-3  m-1 px-md-4 py-1 border-0 rounded-pill"
-                    > database</a
-                  >
-            </div>
-            <div class="col-3 px-0 mx-0 border ">
-                <img src="../assets/images/popular-services/img-1.png" width="100%" height="100%" alt="pic1" >
-            </div>
-            <div class="col-3 p-3 mx-0 border border-gray"> <h4 class="fw-bolder text-black text-uppercase">3d artist</h4>
-                <a
-                    href="#"
-                    class="btn bg-secondary px-3  m-1  px-md-4 py-1 border-0 rounded-pill"
-                    > database</a
-                  >
-                  <a
-                    href="#"
-                    class="btn bg-secondary px-3  m-1  px-md-4 py-1 border-0 rounded-pill"
-                    > database</a
-                  >
-                  <a
-                    href="#"
-                    class="btn bg-secondary px-3  m-1  px-md-4 py-1 border-0 rounded-pill"
-                    > database</a
-                  ></div>
-            <div class="col-3 px-0 mx-0 border border-top-right">
-                <img src="../assets/images/popular-services/img-2.png" width="100%" height="100%" alt="pic2" >
+          <div class="row rounded-top">
+            <div
+              class="col-6 col-lg-3 p-2 p-md-4 pt-3 mx-0 border border-grayed border-top-left order-1"
+            >
+              <h4 class="fw-bolder text-black text-uppercase">3d artist</h4>
+              <a
+                href="#"
+                class="btn bg-white text-black px-3 m-1 px-md-4 py-1 border border-gray rounded-pill f-14"
+              >
+                UX/UI
+              </a>
+              <a
+                href="#"
+                class="btn bg-white text-black px-3 m-1 px-md-4 py-1 border border-gray rounded-pill f-14"
+              >
+                Web design</a
+              >
+              <a
+                href="#"
+                class="btn bg-white text-black px-3 m-1 px-md-4 py-1 border border-gray rounded-pill f-14"
+              >
+                Graphic design</a
+              >
 
+              <div class="justify-content-end d-md-flex d-none">
+                <a
+                  href="#"
+                  class="btn bg-black mx-1 border border-black rounded-circle circled"
+                  ><img
+                    src="../assets/images/popular-services/svg/arrow-up-right-svgrepo-com.svg"
+                    width="100%"
+                    height="100%"
+                    alt="pic"
+                /></a>
+              </div>
             </div>
+            <div class="col-6 col-lg-3 px-0 mx-0 border-0 order-2">
+              <img
+                src="../assets/images/popular-services/img-1.png"
+                width="100%"
+                height="100%"
+                alt="pic1"
+              />
+            </div>
+            <div
+              class="col-6 col-lg-3 p-2 p-md-4 pt-3 mx-0 border border-grayed order-4 order-lg-3"
+            >
+              <h4 class="fw-bolder text-black text-uppercase">
+                Video Explainer
+              </h4>
+              <a
+                href="#"
+                class="btn bg-white text-black px-3 m-1 px-md-4 py-1 border border-gray rounded-pill f-14"
+              >
+                Animation</a
+              >
+              <a
+                href="#"
+                class="btn bg-white text-black px-3 m-1 px-md-4 py-1 border border-gray rounded-pill f-14"
+              >
+                AI
+              </a>
+              <a
+                href="#"
+                class="btn bg-white text-black px-3 m-1 px-md-4 py-1 border border-gray rounded-pill f-14"
+              >
+                database</a
+              >
+              <div class="justify-content-end d-md-flex d-none">
+                <a
+                  href="#"
+                  class="btn bg-black mx-1 border border-black rounded-circle circled"
+                  ><img
+                    src="../assets/images/popular-services/svg/arrow-up-right-svgrepo-com.svg"
+                    width="100%"
+                    height="100%"
+                    alt="pic"
+                /></a>
+              </div>
+            </div>
+            <div class="col-6 col-lg-3 px-0 mx-0 order-3 order-lg-4">
+              <img
+                src="../assets/images/popular-services/img-2.png"
+                width="100%"
+                height="100%"
+                alt="pic2"
+                class="border-top-right"
+              />
+            </div>
+          </div>
+
+          <div class="row rounded-top">
+            <div class="col-6 col-lg-3 px-0 mx-0 border-0 order-1">
+              <img
+                src="../assets/images/popular-services/img-3.png"
+                width="100%"
+                height="100%"
+                alt="pic1"
+                class="border-bottom-left"
+              />
+            </div>
+            <div
+              class="col-6 col-lg-3 p-2 p-md-4 pt-3 mx-0 border border-grayed order-2"
+            >
+              <h4 class="fw-bolder text-black text-uppercase">Graphi design</h4>
+              <a
+                href="#"
+                class="btn bg-white text-black px-3 m-1 px-md-4 py-1 border border-gray rounded-pill f-14"
+              >
+                photoshoh</a
+              >
+              <a
+                href="#"
+                class="btn bg-white text-black px-3 m-1 px-md-4 py-1 border border-gray rounded-pill f-14"
+              >
+                design</a
+              >
+              <a
+                href="#"
+                class="btn bg-white text-black px-3 m-1 px-md-4 py-1 border border-gray rounded-pill f-14"
+              >
+                illustator</a
+              >
+
+              <div
+                class="justify-content-end d-md-flex d-none border-botom-left"
+              >
+                <a
+                  href="#"
+                  class="btn bg-black mx-1 border border-black rounded-circle circled"
+                  ><img
+                    src="../assets/images/popular-services/svg/arrow-up-right-svgrepo-com.svg"
+                    width="100%"
+                    height="100%"
+                    alt="pic"
+                /></a>
+              </div>
+            </div>
+            <div class="col-6 col-lg-3 px-0 mx-0 order-4 order-lg-3">
+              <img
+                src="../assets/images/popular-services/img-4.png"
+                width="100%"
+                height="100%"
+                alt="pic2"
+              />
+            </div>
+            <div
+              class="col-6 col-lg-3 p-2 p-md-4 pt-3 mx-0 border border-grayed order-3 order-lg-4 border-bottom-right"
+            >
+              <h4 class="fw-bolder text-black text-uppercase">
+                Digital Marketing
+              </h4>
+              <a
+                href="#"
+                class="btn bg-white text-black px-3 m-1 px-md-4 py-1 border border-gray rounded-pill f-14"
+              >
+                Advertisement</a
+              >
+              <a
+                href="#"
+                class="btn bg-white text-black px-3 m-1 px-md-4 py-1 border border-gray rounded-pill f-14"
+              >
+                marketing</a
+              >
+              <a
+                href="#"
+                class="btn bg-white text-black px-3 m-1 px-md-4 py-1 border border-gray rounded-pill f-14"
+              >
+                database</a
+              >
+              <div class="justify-content-end d-md-flex d-none">
+                <a
+                  href="#"
+                  class="btn bg-black mx-1 border border-black rounded-circle circled"
+                  ><img
+                    src="../assets/images/popular-services/svg/arrow-up-right-svgrepo-com.svg"
+                    width="100%"
+                    height="100%"
+                    alt="pic"
+                /></a>
+              </div>
+            </div>
+          </div>
         </div>
-        </div>
       </div>
-      </div>
-      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
---
+++ src/components/talent.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container-fluid bg-primary pb-5">
+    <div class="container px-3 px-sm-0 w-100 align-items-center">
+      <div class="row">
+
+        
+      </div>
+    </div>
+  </div>
+</template>
