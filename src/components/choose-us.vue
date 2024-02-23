dc46effd644666f258cb09cd43f578858e5a470a
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-02-23 19:24:08 +0500

    choose us

--- src/components/choose-us.vue
+++ src/components/choose-us.vue
@@ -15,45 +15,152 @@
             creativity of its creator.
           </div>
         </div>
-
-        <div class="col-12 col-lg-6 d-none d-lg-inline" >
-            <img src="../assets/images/choose-us/choose us.png" width="100%" height="100%" alt="choose us pic" >
+        <div class="col-12 col-lg-6 d-none d-lg-inline">
+          <img
+            src="../assets/images/choose-us/choose us.png"
+            width="100%"
+            height="100%"
+            alt="choose us pic"
+          />
         </div>
-        <div class="col-12 col-lg-6" >
-<div class="row">
-<div class="col-12">
-    <div class="card bg-white  border-0">
-            <div class="card-body p-3 pb-0">
-              <div class="row">
-               <div class="col-2 d-none d-md-inline"></div>
-                <div class="col-12 col-md-10  rounded-4 py-3">
-                  <img src="../assets/images//choose-us/collabration.png" class="float-start me-3">
-                  <h6 class="fw-bold text-uppercase">Seamless collabration</h6>
-                  <p > The outstanding workmanship displayed in a piece of art,
-                    craftsmanship</p>
-                </div>
-                <div class="col-12 col-md-10 bg-secondary rounded-4 py-3">
-                  <img src="../assets/images//choose-us/support.png" width="auto" height="auto" alt="support" class="float-start me-3">
-                  <div>    <h6 class="fw-bold text-uppercase">Support and community</h6>
-                  <p class="mb-0 " > The outstanding workmanship displayed in a piece of art,
-                    craftsmanship.</p></div>
-                </div>
-                <div class="col-2 d-none d-md-inline"></div>
-                <div class="col-2 d-none d-md-inline"></div>
+        <div class="col-12 col-lg-6">
+          <div class="row">
+            <div class="col-12 pt-5">
+              <div class="card bg-white border-0 pt-0 pt-lg-5">
+                <div class="card-body p-3 pb-0">
+                  <div class="row">
+                    <div class="col-2 d-none d-md-inline"></div>
+                    <div class="col-12 col-md-10 rounded-4 py-3">
+                      <img
+                        src="../assets/images//choose-us/collabration.png"
+                        class="float-start me-3"
+                      />
+                      <h6 class="fw-bold text-uppercase">
+                        Seamless collabration
+                      </h6>
+                      <p>
+                        The outstanding workmanship displayed in a piece of art,
+                        craftsmanship
+                      </p>
+                    </div>
+                    <div class="col-12 col-md-10 bg-secondary rounded-4 py-3">
+                      <img
+                        src="../assets/images//choose-us/support.png"
+                        width="auto"
+                        height="auto"
+                        alt="support"
+                        class="float-start me-3"
+                      />
+                      <div>
+                        <h6 class="fw-bold text-uppercase">
+                          Support and community
+                        </h6>
+                        <p class="mb-0">
+                          The outstanding workmanship displayed in a piece of
+                          art, craftsmanship.
+                        </p>
+                      </div>
+                    </div>
+                    <div class="col-2 d-none d-md-inline"></div>
+                    <div class="col-2 d-none d-md-inline"></div>
 
-                <div class="col-12 col-md-10  rounded-4 py-3">
-                  <img src="../assets/images//choose-us/secure.png" class="float-start me-3">
-                  <h6 class="fw-bold text-uppercase ">Secure and reliable</h6>
-                  <p class="mb-0" > The outstanding workmanship displayed in a piece of art,
-                    craftsmanship.</p>
+                    <div class="col-12 col-md-10 rounded-4 py-3">
+                      <img
+                        src="../assets/images//choose-us/secure.png"
+                        class="float-start me-3"
+                      />
+                      <h6 class="fw-bold text-uppercase">
+                        Secure and reliable
+                      </h6>
+                      <p class="mb-0">
+                        The outstanding workmanship displayed in a piece of art,
+                        craftsmanship.
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-</div>
-<div class="col-10"></div>
-<div class="col-10"></div>
-</div>
+        </div>
+        <div class="col-12">
+          <div class="card rounded-5 bg-ternary">
+            <div class="card-body p-3 pb-0">
+              <div class="row">
+                <div class="col-12 col-lg-6 ps-3 ps-md-5 pb-3">
+                  <h1 class="fw-bolder text-uppercase text-black f-52 mt-3">
+                    What our customer says
+                  </h1>
+                  <div class="justify-content-start d-flex w-50 float-start">
+                    <a
+                      href="#"
+                      class="btn bg-black mx-1 border border-black rounded-circle circled"
+                      ><img
+                        src="../assets/images/popular-services/svg/left-arrow.svg"
+                        width="100%"
+                        height="100%"
+                        alt="pic"
+                    /></a>
+                    <a
+                      href="#"
+                      class="btn bg-white mx-1 border border-black rounded-circle circled"
+                      ><img
+                        src="../assets/images/popular-services/svg/right-arrow.svg"
+                        width="100%"
+                        height="100%"
+                        alt="pic"
+                      />
+                    </a>
+                  </div>
+                  <div
+                    class="justify-content-end w-50 float-end d-none d-xl-flex"
+                  >
+                    <a href="#" class="btn mt-3 mx-0 px-0 border-0"
+                      ><img
+                        src="../assets/images/choose-us/crul-arrow.png"
+                        width="100%"
+                        height="100%"
+                        alt="pic"
+                    /></a>
+                  </div>
+                </div>
+                <div class="col-12 col-lg-6 py-0 mt-3">
+                  <img
+                    src="../assets/images/choose-us/quatations.png"
+                    width="auto"
+                    height="auto"
+                    alt="pic"
+                  />
+                  <div class="text-black ps-5">
+                    The outstanding workmanship displayed in a piece of art,
+                    craftsmanship, or any creation is often a testament to the
+                    skill, dedication, and creativity of its creator. It
+                    encompasses the meticulous attention to detail, precision,
+                    and mastery of techniques that elevate the work from
+                    ordinary to extraordinary."
+                  </div>
+                  <div class="w-50 float-start">
+                    <img
+                      src="../assets/images/choose-us/profile.png"
+                      width="auto"
+                      height="auto"
+                      alt="pic"
+                      class="my-3 float-start"
+                    />
+                    <h6 class="fw-bold text-uppercase mt-4 mb-0">Thomas</h6>
+                    <div class="ps-5">CEO at Cakar</div>
+                  </div>
+                  <img
+                    src="../assets/images/choose-us/logo-cakar.png"
+                    width="auto"
+                    height="auto"
+                    alt="pic"
+                    class="my-3 float-start"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
