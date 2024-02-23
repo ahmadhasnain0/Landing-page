ae99a530bdafbd7e3d420b0adf22a5532254893d
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-02-23 16:52:13 +0500

    talent

--- src/components/talent.vue
+++ src/components/talent.vue
@@ -2,31 +2,41 @@
   <div class="container-fluid white pb-5">
     <div class="container px-3 px-sm-0 w-100 align-items-center">
       <div class="row">
-
         <div class="col-12">
-<img src="../assets/images/talent/talent-pic.png" width="100%" height="100%" alt="talent-pic" class="object-fit-contain">
-<div class="card rounded-5 bg-primary mt-m10" >
-      <div class="card-body p-3 pb-0">
-        <div class="row">
-            <div class="col-12 col-lg-6 ">
-                <a
+          <img
+            src="../assets/images/talent/talent-pic.png"
+            width="100%"
+            height="100%"
+            alt="talent-pic"
+            class="object-fit-contain"
+          />
+          <div class="card rounded-5 bg-primary mt-m10">
+            <div class="card-body p-3 pb-0">
+              <div class="row">
+                <div class="col-12 col-lg-6">
+                  <a
                     href="#"
-                    class="btn bg-secondary px-3 px-md-4 py-1 text-black border-0 fw-bold rounded-pill"   
-                    > For Talent</a
+                    class="btn bg-secondary px-3 px-md-4 py-1 text-black border-0 fw-bold rounded-pill"
                   >
-                  
+                    For Talent</a
+                  >
                   <h1 class="fw-bolder text-uppercase text-black f-62 mt-3">
-Find Outstandin sportmanship. </h1>
+                    Find Outstandin sportmanship.
+                  </h1>
+                </div>
+                <div class="col-12 col-lg-6 py-0 py-lg-5">
+                  <div class="py-3 text-black">
+                    The outstanding workmanship displayed in a piece of art,
+                    craftsmanship, or any creation is often a testament to the
+                    skill, dedication, and creativity of its creator. It
+                    encompasses the meticulous attention to detail, precision,
+                    and mastery of techniques that elevate the work from
+                    ordinary to extraordinary.
                   </div>
-            <div class="col-12 col-lg-6  py-0 py-lg-5">
-                <div class="py-3 text-black" >The outstanding workmanship displayed in a piece of art, craftsmanship, or any creation is often a testament to the skill, dedication, and creativity of its creator. It encompasses the meticulous attention to detail, precision, and mastery of techniques that elevate the work from ordinary to extraordinary.</div>
-
+                </div>
+              </div>
             </div>
-        </div>
-
-
-    </div>
-</div>
+          </div>
         </div>
       </div>
     </div>
